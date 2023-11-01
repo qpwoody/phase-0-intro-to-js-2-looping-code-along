df9429ca239a2f45780a5d8c253099b98a84eb92
quinn woodhouse <qpwoodhouse@gmail.com>
2023-11-01 13:50:31 -0600

    looping lab code along

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+
+let thankYouMessages = writeCards(names)
+console.log(thankYouMessages)
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
